d14008b947478ff98c0e0ccf73a462b95eaf3e25
Amanda Smith <ajs.amandasmith@gmail.com>
2022-07-26 14:32:48 -0500

    commit tree b336c17ddc21ebdd7d24b473e0815f422368c66c

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    empty_array = []
+    sorted_word = @word.chars.sort
+    array.map do |word|
+      current_sorted_word = word.chars.sort
+      # binding.pry
+      if current_sorted_word == sorted_word
+        empty_array << word
+        # binding.pry
+        # return empty_array
+      end
+    end
+    return empty_array
+    # binding.pry
+  end
+end
+
+listen = Anagram.new("listen")
+listen.match(%w[enlists google inlets banana])
